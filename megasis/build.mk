a73f81bf9d00118337c108baa847e745557ddef8
Tero Hasu <tero.hasu@hut.fi>
2010-06-22 20:07:18 +0300

    megasis pkg template fix

--- megasis/build.mk
+++ megasis/build.mk
@@ -1,40 +1,44 @@
+SAKE := ../tools/bin/sake
+
 CURRENT_CONFIG := ../daemon/src/current_config.mk
 
 include $(CURRENT_CONFIG)
 
 # This is quite important in case we must re-sign something.
+# Note that the libs-s60 directory is not a part of the distribution.
+# It should contain all the embedded third-party SIS files for the megaSIS.
 external :
-	cd ../libs-s60 && make build
+	cd ../libs-s60 && $(MAKE) build
 
 # Currently we only do UDEB builds, and so we do not require UREL builds
 # of static libs yet either.
 static_libs :
-	cd ../sqlite3h && sake static=true kits=$(KIT_NAME) udeb=true cert=dev
+	cd ../sqlite3h && $(SAKE) static=true kits=$(KIT_NAME) udeb=true cert=dev
 
 s60_30_self30 :
-	cd ../daemon && make
-	cd ../cxx-cl2-cli-lib && sake cert=self30 kits=s60_30
-	cd ../py-cl2-cli-lib && sake cert=self30 kits=s60_30
-	cd ../launcher && make CERT=self30 sis
+	cd ../daemon && $(MAKE)
+	cd ../cxx-cl2-cli-lib && $(SAKE) cert=self30 kits=s60_30
+	cd ../py-cl2-cli-lib && $(SAKE) cert=self30 kits=s60_30
+	cd ../launcher && $(MAKE) CERT=self30 sis
 
 s60_30_dev_sub :
-	cd ../daemon && make
-	cd ../cxx-cl2-cli-lib && sake cert=dev kits=s60_30
-	cd ../py-cl2-cli-lib && sake cert=dev kits=s60_30
-	cd ../keyevents && sake cert=dev kits=s60_30
-	cd ../watchdog && make
+	cd ../daemon && $(MAKE)
+	cd ../cxx-cl2-cli-lib && $(SAKE) cert=dev kits=s60_30
+	cd ../py-cl2-cli-lib && $(SAKE) cert=dev kits=s60_30
+	cd ../keyevents && $(SAKE) cert=dev kits=s60_30
+	cd ../watchdog && $(MAKE)
 
 s60_30_dev : s60_30_dev_sub
-	cd ../launcher && make CERT=dev sis
+	cd ../launcher && $(MAKE) CERT=dev sis
 
 s60_30_unsigned : s60_30_dev_sub
-	cd ../launcher && make CERT=unsigned sis
+	cd ../launcher && $(MAKE) CERT=unsigned sis
 
 s60_30_self32 :
-	cd ../daemon && make
-	cd ../cxx-cl2-cli-lib && sake cert=self32 kits=s60_30
-	cd ../py-cl2-cli-lib && sake cert=self32 kits=s60_30
-	cd ../launcher && make CERT=self32 sis
+	cd ../daemon && $(MAKE)
+	cd ../cxx-cl2-cli-lib && $(SAKE) cert=self32 kits=s60_30
+	cd ../py-cl2-cli-lib && $(SAKE) cert=self32 kits=s60_30
+	cd ../launcher && $(MAKE) CERT=self32 sis
 
 non-use : s60_30_dev
 
