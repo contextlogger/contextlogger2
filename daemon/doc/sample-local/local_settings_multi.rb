9be6d360ec42481e4992ed1a2b3533831afc1879
Tero Hasu <tero.hasu@hut.fi>
2010-07-06 16:26:13 +0300

    new, more complex settings example

--- daemon/doc/sample-local/local_settings_multi.rb
+++ daemon/doc/sample-local/local_settings_multi.rb
@@ -2,7 +2,7 @@
 
 Customize this Ruby library to reflect your local Symbian setup, and define an environment variable named EPOCLOCALRB to specify the "require" pathname of the library. (It should be the full path unless it is under your RUBYLIB path.) In other words, tools will import this library with "require ENV['EPOCLOCALRB']".
 
-This simple example uses the included self-signed cert, and assumes that one is using a particular SDK for all targets, and that the environment settings for it have been predefined.
+This more complex example uses the included self-signed cert as in the _self example, but assumes that one is using different SDKs for different targets, set up in a custom way.
 
 =end
 
@@ -25,6 +25,38 @@ module EpocLocalRb
   def sh cmd
     puts cmd
     system(cmd) or raise "command #{cmd.inspect} failed"
+  end
+
+  def set_env_var name, value
+    if value
+      puts "set #{name}=#{value}"
+    else
+      puts "unset #{name}"
+    end
+    ENV[name] = value
+  end
+
+  def set_env m
+    m.each do |key, value|
+      set_env_var(key, value)
+    end
+  end
+
+  def with_env m, block
+    old_m = {}
+    m.each_key do |key|
+      old_m[key] = ENV[key]
+    end
+    begin
+      m.each do |key, value|
+        set_env_var(key, value)
+      end
+      block.call
+    ensure
+      old_m.each do |key, value|
+        set_env_var(key, value)
+      end
+    end
   end
 
   extend self
@@ -50,15 +82,68 @@ def epoc_cert_info cert, plat
   EpocLocalRb::CertInfo.new(*args)
 end
 
+# In this case, to target a particular platform, we just switch to using a kit tailored for that platform. We also set the KITNAME env var in case other code or programs must somehow adapt for the platform.
+def EpocLocalRb::epoc_make_env plat
+  gnupocdevenv = ENV['GNUPOCDEVENV']
+  unless gnupocdevenv
+    raise "GNUPOCDEVENV must be set when using GnuPoc"
+  end
+
+  m = Hash.new
+  m['KITNAME'] = kit = plat
+  m['EPOCROOT'] = epocroot = "#{gnupocdevenv}/kits/#{kit}/"
+
+  path = ENV['PATH']
+
+  if File.exist? "#{epocroot}epoc32/tools/elf2e32.exe"
+    if ENV['EPOCGCC']
+      epocgcc = ENV['EPOCGCC']
+    else
+      m['EPOCGCC'] = epocgcc = "#{gnupocdevenv}/gcce-2005q1"
+      m['EPOCGCCVER'] = '3.4.3'
+    end
+    gcctools = "#{epocgcc}/bin"
+    ekatools = "#{gnupocdevenv}/eka2/bin"
+    path = [gcctools,
+            "#{epocroot}epoc32/tools",
+            path].join(":")
+    if File.exist? "#{gnupocdevenv}/qt-latest"
+      path = ["#{gnupocdevenv}/qt-latest/bin", path].join(":")
+    end
+  else
+    if ENV['EPOCGCC']
+      epocgcc = ENV['EPOCGCC']
+    else
+      ENV['EPOCGCC'] = epocgcc = "#{gnupocdevenv}/gcc"
+      ENV['EPOCGCCVER'] = "2.9-psion-98r2"
+    end
+    gcctools = "#{epocgcc}/bin"
+    ekatools = "#{gnupocdevenv}/eka1/bin"
+    path = [gcctools,
+            "#{epocroot}epoc32/tools",
+            path].join(":")
+  end
+
+  path = ["#{gnupocdevenv}/bin", path].join(":")
+  m['PATH'] = path
+
+  return m
+end
+
 # This function must be defined.
+#
+# If you need to set EPOCROOT or whatever to build for a particular target platform, you may do so here.
 def epoc_set_plat_env plat
+  m = EpocLocalRb::epoc_make_env plat
+  EpocLocalRb::set_env(m)
 end
 
 # This function must be defined.
 #
 # If you need to set EPOCROOT or whatever to build for a particular target platform, you may do so here. Otherwise just yield.
-def epoc_in_plat_env plat # block
-  yield
+def epoc_in_plat_env plat, &block
+  m = EpocLocalRb::epoc_make_env plat
+  EpocLocalRb::with_env(m, block)
 end
 
 #
