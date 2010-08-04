416eda982c8fa008fdfcb641f899c64d91a39621
Tero Hasu <tero.hasu@hut.fi>
2010-08-05 00:53:59 +0300

    upload receive php script

---
+++ upload-php/upload.php
@@ -0,0 +1,98 @@
+<?php
+
+$dateStr=date("Y-m-d-H-i-s");
+
+$logHome = "../../data/cl2";
+
+$f=fopen($logHome . "/uploadlog.txt","a");
+dbgPrint("\n".$dateStr."------------------------------------------",$f);
+
+function dbgPrint($obj,$f) {
+  echo "\n";
+  print_r($obj);
+  echo "\n";
+
+  if ($f) { 
+    fwrite($f,print_r($obj,true)."\n");
+  }
+}
+
+if ( isset($_FILES) &&
+     isset($_FILES["logdata"]) &&
+     isset($_FILES["logdata"]["tmp_name"]) &&
+     isset($_FILES["logdata"]["name"]) ) {
+
+  dbgPrint("HTTP upload fields in \$_FILES were correct",$f);
+  
+  if($file["error"] == UPLOAD_ERR_OK) {
+    $logTmpName=$_FILES['logdata']['tmp_name'];
+    $logName=$_FILES['logdata']['name'];
+
+    dbgPrint("HTTP upload status was OK",$f);
+    
+    $matches=array();
+    if ( preg_match("/^([_a-zA-Z][_a-zA-Z0-9]*)[.]db$/", $logName, $matches)==1 ) {
+      dbgPrint("Uploading user's identity analyzed (".$matches[1].")",$f);
+
+      if ( strlen($matches[1])>10 ) {
+        $matches[1]=substr($matches[1],0,10);
+        dbgPrint("User name length exceeded 10 characters. Name trimmed to 10 chars (".$matches[1].")",$f);
+      }
+
+      $logFileName="log--" . $matches[1] . "--".$dateStr.".db";
+      $logFullPath = $logHome."/".$logFileName;
+      move_uploaded_file($logTmpName, $logFullPath)
+        or die("mv");
+
+      dbgPrint("Raw log file stored successfully in " . $logFullPath, $f);
+    }
+    else {
+      header("Status: 400 Bad request",true,400);
+      dbgPrint("ERROR: could not identify username from logName=".$logName.". Matches were:\n".print_r($matches,true),$f);      
+    }
+  }
+  else {
+    header("Status: 400 Bad request",true,400);
+    dbgPrint("ERROR in upload, error code=".$file["error"].
+             "\n\$_FILES=".print_r($_FILES,true),$f);
+  }
+}
+else {
+  header("Status: 400 Bad request",true,400);
+  dbgPrint("ERROR in \$_FILES. \$_SERVER=".print_r($_SERVER,true).", \$_FILES=".print_r($_SERVER,true),$f);
+}
+
+dbgPrint("Closing the log file",$f);
+fclose($f);
+
+/**
+
+Copyright 2010 Helsinki Institute for Information Technology (HIIT)
+and the authors. All rights reserved.
+
+Authors: Antti Salovaara <antti.salovaara@hiit.fi>
+         Tero Hasu <tero.hasu@hut.fi>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ **/
+
+?>
