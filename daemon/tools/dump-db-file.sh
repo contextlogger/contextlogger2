7da3512343053ca152861c82036bd418490e4bec
Tero Hasu <tero.hasu@hut.fi>
2010-09-13 22:18:16 +0300

    dump tool accepts filename on cli

--- daemon/tools/dump-db-file.sh
+++ daemon/tools/dump-db-file.sh
@@ -1,55 +1,55 @@
 echo APPLICATION FOCUS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), uid, caption from appfocus_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), uid, caption from appfocus_scan;"
 
 echo BATTERY STATUS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), status, level from battery_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), status, level from battery_scan;"
 echo NETWORK REGISTRATION STATUS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), status from registration_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), status from registration_scan;"
 echo NETWORK SIGNAL STRENGTH
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), dbm, bars from signal_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), dbm, bars from signal_scan;"
 echo OPERATOR
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), name from operator_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), name from operator_scan;"
 
 echo BLUETOOTH PROXIMITY DEVICES
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), address, name from btprox_item, btprox_scan where btprox_item.scan_id = btprox_scan.scan_id order by unixtime, address;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), address, name from btprox_item, btprox_scan where btprox_item.scan_id = btprox_scan.scan_id order by unixtime, address;"
 echo BLUETOOTH PROXIMITY SCAN TIMES
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), scan_id from btprox_scan order by unixtime;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), scan_id from btprox_scan order by unixtime;"
 
 echo GSM CELL ID
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), country_code, network_code, area_code, cell_id from cellid_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), country_code, network_code, area_code, cell_id from cellid_scan;"
 
 echo FLIGHT MODE
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), value from flightmode_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), value from flightmode_scan;"
 
 echo STATUS INDICATORS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), value, caps from indicator_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), value, caps from indicator_scan;"
 
 echo CALL STATUS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), value, number, contact_name, datetime(starttime, 'unixepoch'), osterm, netterm from callstatus_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), value, number, contact_name, datetime(starttime, 'unixepoch'), osterm, netterm from callstatus_scan;"
 
 echo SMS EVENT
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), evtype, number, contact_name from smsevent_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), evtype, number, contact_name from smsevent_scan;"
 
 echo INACTIVITY '(active=1)'
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), value from inactivity_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), value from inactivity_scan;"
 
 echo GPS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), latitude, longitude, altitude, vertical_accuracy, horizontal_accuracy, course, satellites from gps_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), latitude, longitude, altitude, vertical_accuracy, horizontal_accuracy, course, satellites from gps_scan;"
 
 echo PROFILE CHANGE
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), value, name from profile_scan;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), value, name from profile_scan;"
 
 echo KEYPRESS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), presstimes from keypress_scan;" 
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), presstimes from keypress_scan;" 
 
 echo APP MESSAGES
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), message from appmessage_log;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), message from appmessage_log;"
 
 echo STATUS MESSAGES
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), message from status_log;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), message from status_log;"
 
 echo MARKS
-sqlite3 log.db "select datetime(unixtime, 'unixepoch'), message from mark_log;"
+sqlite3 $1 "select datetime(unixtime, 'unixepoch'), message from mark_log;"
 
 echo Notes:
 echo Text is encoded as UTF-8.
