26ad666d241edfd586b538548858e7b2e3ecff32
Tero Hasu <tero.hasu@hut.fi>
2011-01-03 16:34:41 +0200

    cl2webfilter

---
+++ filter-http/sakefile.rb
@@ -0,0 +1,179 @@
+# -*- ruby -*-
+# sake variant sake4
+
+require 'sake4/component'
+
+$projname = $sake_op[:projname]
+$basename = "cl2webfilter"
+$uid = $sake_op[:uid].hex
+$version = $sake_op[:version].split(".").map {|x| x.to_i}
+$caps = $sake_op[:caps].split(/\s+/)
+
+uidobj = Sake::Uid.new($uid, $uid)
+
+$proj = Sake::Project.new(:basename => $basename,
+                          :name => $projname,
+                          :version => $version,
+                          :uid => uidobj)
+
+$app = Sake::Component.new(:project => $proj,
+                           :target_type => :dll,
+                           :basename => $basename,
+                           :bin_basename => $sake_op[:bin_basename],
+                           :uid3 => uidobj,
+                           :caps => $caps)
+
+class <<$app
+  def mmp_in_file
+    group_dir + ("module.mmp.in")
+  end
+end
+
+$comp_list = [$app].compact
+
+if $sake_op[:kits]
+  $kits = Sake::DevKits::get_exact_set($sake_op[:kits].strip.split(/,/))
+else
+  $kits = Sake::DevKits::get_all
+end
+
+if $sake_op[:comps]
+  comps = $sake_op[:comps].strip.split(/,/)
+  $comp_list.delete_if do |comp|
+    !comps.include?(comp.basename)
+  end
+end
+
+$builds = $kits.map do |kit|
+  build = Sake::ProjBuild.new(:project => $proj,
+                              :devkit => kit)
+  build.abld_platform = (build.v9_up? ? "gcce" : "armi")
+  build.abld_build = ($sake_op[:rel] || raise)
+  build.handle = ($sake_op[:handle] || raise)
+  build
+end
+
+def try_load file
+  begin
+    load file
+  rescue LoadError; end
+end
+
+if $sake_op[:builds]
+  blist = $sake_op[:builds]
+  $builds.delete_if do |build|
+    !blist.include?(build.handle)
+  end
+end
+
+$builds.sort! do |x,y|
+  x.handle <=> y.handle
+end
+
+desc "Prints a list of possible builds."
+task :builds do
+  for build in $builds
+    puts build.handle
+  end
+end
+
+desc "Prints info about possible builds."
+task :build_info do
+  for build in $builds
+    puts "#{build.handle}:"
+    puts "  project name  #{build.project.name}"
+    puts "    basename    #{build.project.basename}"
+    puts "  target        #{build.target.handle}"
+    puts "  devkit        #{build.devkit.handle}"
+    puts "  abld platform #{build.abld_platform}"
+    puts "  abld build    #{build.abld_build}"
+    puts "  cert caps     #{$comp_build.caps.inspect}"
+    puts "  components    #{build.comp_builds.map {|x| x.component.basename}.inspect}"
+  end
+end
+
+class HexNum
+  def initialize num
+    @num = num
+  end
+
+  def to_s
+    "0x%08x" % @num
+  end
+end
+
+$exeb = Hash.new
+for build in $builds
+  map = build.trait_map
+
+  # To define __UID__ for header files.
+  if build.uid
+    map[:uid] = HexNum.new(build.uid.number)
+  end
+
+  # NDEBUG controls whether asserts are to be compiled in (NDEBUG is
+  # defined in UDEB builds). Normally an assert results in something
+  # being printed to the console. To also have the errors logged, you
+  # will want to enable logging by setting "logging=true". Without
+  # this setting, there will be no dependency on the (deprecated) file
+  # logger API, and errors are still displayed on the console (if you
+  # have one and have time to read it). "logging=true" has no effect
+  # if your SDK does not have the required API.
+  if $sake_op[:logging] and map[:has_flogger]
+    map[:do_logging] = :define
+  end
+
+  # Each build variant shall have all of the components.
+  build.comp_builds = $comp_list.map do |comp|
+    b = Sake::CompBuild.new(:proj_build => build,
+                            :component => comp)
+    b.caps = $caps
+    $comp_build = b
+    b
+  end
+end
+
+task :default => [:bin]
+
+require 'sake4/tasks'
+
+Sake::Tasks::def_list_devices_tasks(:builds => $builds)
+
+Sake::Tasks::def_makefile_tasks(:builds => $builds)
+
+Sake::Tasks::def_binary_tasks(:builds => $builds)
+
+Sake::Tasks::def_clean_tasks(:builds => $builds)
+
+Sake::Tasks::force_uncurrent_on_op_change
+
+=begin rdoc
+
+Copyright 2010 Helsinki Institute for Information Technology (HIIT)
+and the authors. All rights reserved.
+
+Authors: Tero Hasu <tero.hasu@hut.fi>
+
+= License
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=end
---
+++ filter-http/src/module.cpp
@@ -0,0 +1,112 @@
+// This code based on an example retrieved from http://wiki.forum.nokia.com/index.php/Writing_an_HTTP_filter_plugin
+
+#include <e32base.h>
+#include <e32debug.h>
+#include <ecom/ecom.h>
+#include <ecom/implementationproxy.h>
+#include <http/cecomfilter.h>
+#include <http/mhttpfilter.h>
+#include <http/rhttptransaction.h>
+
+_LIT8(KCl2WebFilterName, "cl2webfilter");
+
+class CCl2WebFilter : 
+  public CEComFilter, 
+  public MHTTPFilter
+{
+public:
+  static CEComFilter* CreateFilterL(TAny* aHttpSession);
+	
+  // virtuals from MHTTPFilter
+  void MHFUnload(RHTTPSession aSession, THTTPFilterHandle aHandle);
+  void MHFLoad(RHTTPSession aSession, THTTPFilterHandle aHandle);
+  void MHFRunL(RHTTPTransaction aTransaction, const THTTPEvent& aEvent);
+  void MHFSessionRunL(const THTTPSessionEvent& aEvent);
+  TInt MHFRunError(TInt aError, RHTTPTransaction aTransaction, const THTTPEvent& aEvent);
+  TInt MHFSessionRunError(TInt aError, const THTTPSessionEvent& aEvent);
+	
+  ~CCl2WebFilter();
+private:
+  void ConstructL(RHTTPSession& aHttpSession);
+	
+private:
+  RStringF iFilterName;
+};
+	
+CEComFilter* CCl2WebFilter::CreateFilterL(TAny* aHttpSession)
+{
+  // The CEcomFilter class passes us a pointer to the RHTTPSession so we can install ourselves
+  RHTTPSession* session = reinterpret_cast<RHTTPSession*>(aHttpSession);
+  CCl2WebFilter* self = new (ELeave) CCl2WebFilter;
+  CleanupStack::PushL(self);
+  self->ConstructL(*session);
+  CleanupStack::Pop(self);
+  return self;
+}
+	
+CCl2WebFilter::~CCl2WebFilter()
+{
+  iFilterName.Close();
+}
+	
+void CCl2WebFilter::ConstructL(RHTTPSession& aSession)
+{
+  // install this filter in to the current session
+  iFilterName = aSession.StringPool().OpenFStringL(KCl2WebFilterName);
+  aSession.FilterCollection().AddFilterL(*this, THTTPEvent::EAnyTransactionEvent,
+					 RStringF(), KAnyStatusCode, EClientFilters, iFilterName);
+}
+
+void CCl2WebFilter::MHFUnload(RHTTPSession aSession, THTTPFilterHandle aHandle)
+{
+  // called when our filter is unloaded
+  RDebug::Print(_L("Demo Filter: HTTP filter unloaded."));
+  // self loading filters manage their own life cycle...
+  delete this;
+}
+
+void CCl2WebFilter::MHFLoad(RHTTPSession aSession, THTTPFilterHandle aHandle)
+{
+  // called when our filter is loaded.
+  RDebug::Print(_L("Demo Filter: HTTP filter loaded."));
+}
+
+void CCl2WebFilter::MHFRunL(RHTTPTransaction aTransaction, const THTTPEvent& aEvent)
+{
+  // called when a RHTTPTransaction event happens.
+  if (aEvent == THTTPEvent::ESubmit)
+    {
+      const TDesC8& uri = aTransaction.Request().URI().UriDes();
+      RDebug::Printf("Demo Filter: New transaction submitted, URI: %S", &uri);
+    }
+}
+
+void CCl2WebFilter::MHFSessionRunL(const THTTPSessionEvent& aEvent)
+{
+  // called when an RHTTPSession even happens.
+}
+
+TInt CCl2WebFilter::MHFRunError(TInt aError, RHTTPTransaction aTransaction, const THTTPEvent& aEvent)
+{
+  // called when MHFRunL leaves, as with active objects
+  return KErrNone;
+}
+
+TInt CCl2WebFilter::MHFSessionRunError(TInt aError, const THTTPSessionEvent& aEvent)
+{
+  // called when MHFSessionRunL leaves.
+  return KErrNone;
+}
+	
+// The standard ECOM initialisation stuff
+	
+const TImplementationProxy KImplementationTable[] = 
+  {
+    IMPLEMENTATION_PROXY_ENTRY(0xe846000f, CCl2WebFilter::CreateFilterL)
+  };
+
+EXPORT_C const TImplementationProxy* ImplementationGroupProxy(TInt& aTableCount)
+{
+  aTableCount = sizeof(KImplementationTable) / sizeof(TImplementationProxy);
+  return KImplementationTable;
+}
