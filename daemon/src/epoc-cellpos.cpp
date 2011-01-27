4e841d8d5870b63bc7af91b5d53a72f60697412c
Tero Hasu <tero.hasu@hut.fi>
2011-01-27 13:13:25 +0200

    positionereventl more readable

--- daemon/src/epoc-cellpos.cpp
+++ daemon/src/epoc-cellpos.cpp
@@ -199,6 +199,143 @@ void CSensor_cellpos::BbChangedL(bb::RHandle* self, enum bb_DataType dt,
   iPositioner->MakeRequest();
 }
 
+// xxx this could be shared with other satellite sensors
+static void LogSatelliteInfoL(LogDb* aLogDb, const char* aSensorName,
+			      const TPositionSatelliteInfo& positionInfo)
+{
+#define SAT_STRING_MAX 300
+#define COU_STRING_MAX 300
+  gchar satString[SAT_STRING_MAX];
+  gchar couString[COU_STRING_MAX];
+  satString[0] = '\0';
+  couString[0] = '\0';
+
+  if (positionInfo.PositionClassType() & EPositionSatelliteInfoClass) {
+    TInt satellites = positionInfo.NumSatellitesInView();
+    TInt used_satellites = positionInfo.NumSatellitesUsed();
+    TReal32 horizontal_dop = positionInfo.HorizontalDoP();
+    TReal32 vertical_dop = positionInfo.VerticalDoP();
+    TReal32 time_dop = positionInfo.TimeDoP();
+    TTime satTime = positionInfo.SatelliteTime();
+
+#if __DO_LOGGING__
+#define TIME_STRING_MAX 50
+    _LIT(KDateTimeFormat, "%F%Y-%M-%D %H:%T:%S.%C");
+    // Note that KMaxTimeFormatSpec is the max size of the format
+    // spec, and not the result, as I understand it.
+    TBuf<TIME_STRING_MAX> satTime16;
+    // The leave will not happen if the format string and data are
+    // valid and the destination buffer is large enough.
+    satTime.FormatL(satTime16, KDateTimeFormat);
+    gchar satTime8[TIME_STRING_MAX + 1];
+    ConvToUtf8CString(satTime8, TIME_STRING_MAX, satTime16);
+
+    logg("satellite info: num satellites in view %d, num used satellites %d, hdop %g, vdop %g, tdop %g, satellite time %s", satellites, used_satellites, horizontal_dop, vertical_dop, time_dop, satTime8);
+#endif
+
+    // We convert Symbian time to Unix time for consistency.
+    time_t satUnixTime = LocalEpocTimeToUnixTime(satTime); // assuming 32-bit int
+
+    // Build 'satString' for database insertion.
+    {
+      // "satellites": {"horizontal_dop": 2.20000004768372,
+      // "used_satellites": 4, "vertical_dop": 1.0, "time":
+      // 1225980390.097, "satellites": 10, "time_dop": 1.0}
+      GString* gs = NULL;
+      SET_TRAP_OOM(if (gs) g_string_free(gs, TRUE);
+		   User::LeaveNoMemory());
+      gs = g_string_sized_new(128);
+      g_string_printf(gs, "{time: %d", (int)satUnixTime);
+      if (!Math::IsNaN(satellites))
+	g_string_append_printf(gs, ", satellites: %d", satellites);
+      if (!Math::IsNaN(used_satellites))
+	g_string_append_printf(gs, ", used_satellites: %d", used_satellites);
+      if (!Math::IsNaN(horizontal_dop))
+	g_string_append_printf_fix(gs, ", horizontal_dop: %.6f", (double)horizontal_dop);
+      if (!Math::IsNaN(vertical_dop))
+	// %f and %g seem to be a bit broken for this function at
+	// least, and maybe also for the other GLib printf
+	// functions. We can try a more recent Open C, or
+	// implement a replacement using C99 or Symbian functions.
+	// Or possibly we could backport a more recent version of
+	// the function in question from GLib, if there are not
+	// too many internal dependencies.
+	g_string_append_printf_fix(gs, ", vertical_dop: %.6f", (double)vertical_dop);
+      if (!Math::IsNaN(time_dop))
+	g_string_append_printf_fix(gs, ", time_dop: %.6f", (double)time_dop);
+      g_string_append_c(gs, '}');
+      UNSET_TRAP_OOM();
+
+      if (gs->len < SAT_STRING_MAX)
+	// For easier memory management.
+	strcpy(satString, gs->str);
+      else
+	// Did not fit fully, do not log partial data that may not
+	// be parseable.
+	satString[0] = '\0';
+      g_string_free(gs, TRUE);
+      logt(satString);
+    }
+  }
+
+  if (positionInfo.PositionClassType() & EPositionCourseInfoClass) {
+    TCourse course;
+    positionInfo.GetCourse(course);
+    TReal32 speed = course.Speed();
+    TReal32 heading = course.Heading();
+    TReal32 course_value = course.Course();
+    TReal32 speed_accuracy = course.SpeedAccuracy();
+    TReal32 heading_accuracy = course.HeadingAccuracy();
+    TReal32 course_accuracy = course.CourseAccuracy();
+    logg("course info: speed %g m/s, heading %g deg, course %g, speed_accuracy %g m/s, heading_accuracy %d deg, course_accuracy %g", speed, heading, course_value, speed_accuracy, heading_accuracy, course_accuracy);
+
+    // "course": {"speed": 6.0, "heading": 305.880004882812, "heading_accuracy": 5.80999994277954, "speed_accuracy": 8.35999965667725
+    {
+#define APPEND_STRING(s) { int slen = strlen(s); if (space <= slen) goto fail; space -= slen; strcpy(output, s);  output += slen; }
+#define APPEND_SEP { if (!first) APPEND_STRING(", "); }
+#define COU_UPDATE_STATE(r) { if (r < 0 || r >= space) goto fail; space -= r; output += r; first = EFalse; }
+      int space = COU_STRING_MAX;
+      int r;
+      TBool first = ETrue;
+      gchar* output = couString;
+      APPEND_STRING("{");
+      if (!Math::IsNaN(speed)) { r = g_snprintf_fix(output, space, "speed: %.6f", (double)speed); COU_UPDATE_STATE(r); }
+      if (!Math::IsNaN(heading)) { APPEND_SEP; r = g_snprintf_fix(output, space, "heading: %.6f", (double)heading); COU_UPDATE_STATE(r); }
+      if (!Math::IsNaN(course_value)) { APPEND_SEP; r = g_snprintf_fix(output, space, "course: %.6f", (double)course_value); COU_UPDATE_STATE(r); }
+      if (!Math::IsNaN(speed_accuracy)) { APPEND_SEP; r = g_snprintf_fix(output, space, "speed_accuracy: %.6f", (double)speed_accuracy); COU_UPDATE_STATE(r); }
+      if (!Math::IsNaN(heading_accuracy)) { APPEND_SEP; r = g_snprintf_fix(output, space, "heading_accuracy: %.6f", (double)heading_accuracy); COU_UPDATE_STATE(r); }
+      if (!Math::IsNaN(course_accuracy)) { APPEND_SEP; r = g_snprintf_fix(output, space, "course_accuracy: %.6f", (double)course_accuracy); COU_UPDATE_STATE(r); }
+      APPEND_STRING("}");
+      goto done;
+    fail:
+      couString[0] = '\0';
+    done: ;
+      logt(couString);
+    }
+  }
+
+  if (positionInfo.PositionClassType() & EPositionInfoClass) {
+    TPosition position;
+    positionInfo.GetPosition(position);
+    // It seems that with network based positioning we only get
+    // lat and lon and hacc, and the rest are NaN.
+    TReal64 lat = position.Latitude();
+    TReal64 lon = position.Longitude();
+    // To use Google Maps to look these up, can use something like http://maps.google.com/maps?q=60.187537,+24.804940&hl=en
+    TReal32 alt = position.Altitude();
+    TReal32 vacc = position.VerticalAccuracy();
+    TReal32 hacc = position.HorizontalAccuracy();
+    //logg("position info: latitude %lg deg, longitude %lg deg, altitude %g m, vertical_accuracy %g m, horizontal_accuracy %g m", lat, lon, alt, vacc, hacc);
+    guilogf("%s: lat %lg, lon %lg, alt %g, vacc %g m, hacc %g m", aSensorName, lat, lon, alt, vacc, hacc);
+    if (!Math::IsNaN(lat) &&
+	!Math::IsNaN(lon)) {
+      log_db_log_position(aLogDb, lat, lon, alt, vacc, hacc, couString, satString, NULL);
+    } else {
+      logt("supposedly full reading has NaN value(s) in coordinates");
+    }
+  }
+}
+
 gboolean CSensor_cellpos::PositionerEventL(GError** error)
 {
   assert_error_unset(error);
@@ -210,7 +347,7 @@ gboolean CSensor_cellpos::PositionerEventL(GError** error)
   // NotifyPositionUpdate requests. Note that somewhat unusually there
   // are some positive values as well.
   TInt errCode = iPositioner->StatusCode();
-  
+
   if (errCode < 0) {
     // Do not yet quite know what sort of errors might be getting, so
     // shall merely log errors and keep retrying. Do want to make
@@ -227,11 +364,6 @@ gboolean CSensor_cellpos::PositionerEventL(GError** error)
       // SetUpdateTimeOut. If we didn't get the position in that time,
       // then we did not.
       guilogf("cellpos: GPS timeout");
-      break;
-
-    case KPositionPartialUpdate: // partial data
-    case KPositionQualityLoss: // no data
-      guilogf("cellpos: got no GPS data");
       break;
 
       guilogf("cellpos: positioning error");
@@ -268,20 +400,19 @@ gboolean CSensor_cellpos::PositionerEventL(GError** error)
       }
       */
     }
-  } else {
+  } else { // errCode >= 0 (yes, there are _positive_ codes as well)
     iRetryAo->ResetFailures();
-    if (errCode == KPositionQualityLoss) {
-      // The positioning module could not get any information.
-      logt("no gps info available");
-    } else if (errCode == KErrNone || errCode == KPositionPartialUpdate) {
-      // Now this data can get quite complicated and I am not sure it
-      // makes sense for us to start cramming it into a variety of
-      // fields and tables for fancy querying unless we have a clear
-      // requirement to support certain kinds of queries efficiently. So
-      // we shall merely record it all into a single field in some
-      // textual format, at least for the time being. We shall choose
-      // Lua expression syntax for this purpose. Evaluating such strings
-      // can be done with assert(loadstring(s))().
+
+    switch (errCode) { // see <lbserrors.h>
+    case KPositionQualityLoss: // no data
+      {
+	guilogf("cellpos: got no GPS data");
+	break;
+      }
+      
+    case KErrNone: // full data
+    case KPositionPartialUpdate: // partial data
+      {
 #if __DO_LOGGING__
       TBool isPartial = (errCode == KPositionPartialUpdate);
       logg("got %s gps reading", isPartial ? "partial" : "full");
@@ -291,144 +422,19 @@ gboolean CSensor_cellpos::PositionerEventL(GError** error)
       // then if we try to request it we get nothing? Well, we are
       // only choosing GPS modules, and GPS is based on satellites, so
       // surely there always is satellite info.
-
       const TPositionSatelliteInfo& positionInfo = iPositioner->PositionInfo();
 
-#define SAT_STRING_MAX 300
-#define COU_STRING_MAX 300
-      gchar satString[SAT_STRING_MAX];
-      gchar couString[COU_STRING_MAX];
-      satString[0] = '\0';
-      couString[0] = '\0';
-
-      if (positionInfo.PositionClassType() & EPositionSatelliteInfoClass) {
-	TInt satellites = positionInfo.NumSatellitesInView();
-	TInt used_satellites = positionInfo.NumSatellitesUsed();
-	TReal32 horizontal_dop = positionInfo.HorizontalDoP();
-	TReal32 vertical_dop = positionInfo.VerticalDoP();
-	TReal32 time_dop = positionInfo.TimeDoP();
-	TTime satTime = positionInfo.SatelliteTime();
-
-#if __DO_LOGGING__
-#define TIME_STRING_MAX 50
-	_LIT(KDateTimeFormat, "%F%Y-%M-%D %H:%T:%S.%C");
-        // Note that KMaxTimeFormatSpec is the max size of the format
-        // spec, and not the result, as I understand it.
-	TBuf<TIME_STRING_MAX> satTime16;
-        // The leave will not happen if the format string and data are
-        // valid and the destination buffer is large enough.
-	satTime.FormatL(satTime16, KDateTimeFormat);
-	gchar satTime8[TIME_STRING_MAX + 1];
-	ConvToUtf8CString(satTime8, TIME_STRING_MAX, satTime16);
-
-	logg("satellite info: num satellites in view %d, num used satellites %d, hdop %g, vdop %g, tdop %g, satellite time %s", satellites, used_satellites, horizontal_dop, vertical_dop, time_dop, satTime8);
-#endif
-
-	// We convert Symbian time to Unix time for consistency.
-	time_t satUnixTime = LocalEpocTimeToUnixTime(satTime); // assuming 32-bit int
-
-	// Build 'satString' for database insertion.
-	{
-	  // "satellites": {"horizontal_dop": 2.20000004768372,
-	  // "used_satellites": 4, "vertical_dop": 1.0, "time":
-	  // 1225980390.097, "satellites": 10, "time_dop": 1.0}
-	  GString* gs = NULL;
-	  SET_TRAP_OOM(if (gs) g_string_free(gs, TRUE);
-		       User::LeaveNoMemory());
-	  gs = g_string_sized_new(128);
-	  g_string_printf(gs, "{time: %d", (int)satUnixTime);
-	  if (!Math::IsNaN(satellites))
-	    g_string_append_printf(gs, ", satellites: %d", satellites);
-	  if (!Math::IsNaN(used_satellites))
-	    g_string_append_printf(gs, ", used_satellites: %d", used_satellites);
-	  if (!Math::IsNaN(horizontal_dop))
-	    g_string_append_printf_fix(gs, ", horizontal_dop: %.6f", (double)horizontal_dop);
-	  if (!Math::IsNaN(vertical_dop))
-            // %f and %g seem to be a bit broken for this function at
-            // least, and maybe also for the other GLib printf
-            // functions. We can try a more recent Open C, or
-            // implement a replacement using C99 or Symbian functions.
-            // Or possibly we could backport a more recent version of
-            // the function in question from GLib, if there are not
-            // too many internal dependencies.
-	    g_string_append_printf_fix(gs, ", vertical_dop: %.6f", (double)vertical_dop);
-	  if (!Math::IsNaN(time_dop))
-	    g_string_append_printf_fix(gs, ", time_dop: %.6f", (double)time_dop);
-	  g_string_append_c(gs, '}');
-	  UNSET_TRAP_OOM();
-
-	  if (gs->len < SAT_STRING_MAX)
-	    // For easier memory management.
-	    strcpy(satString, gs->str);
-	  else
-	    // Did not fit fully, do not log partial data that may not
-	    // be parseable.
-	    satString[0] = '\0';
-	  g_string_free(gs, TRUE);
-	  logt(satString);
-	}
+      LogSatelliteInfoL(iLogDb, "cellpos", positionInfo);
+      
+      break;
       }
 
-      if (positionInfo.PositionClassType() & EPositionCourseInfoClass) {
-	TCourse course;
-	positionInfo.GetCourse(course);
-	TReal32 speed = course.Speed();
-	TReal32 heading = course.Heading();
-	TReal32 course_value = course.Course();
-	TReal32 speed_accuracy = course.SpeedAccuracy();
-	TReal32 heading_accuracy = course.HeadingAccuracy();
-	TReal32 course_accuracy = course.CourseAccuracy();
-	logg("course info: speed %g m/s, heading %g deg, course %g, speed_accuracy %g m/s, heading_accuracy %d deg, course_accuracy %g", speed, heading, course_value, speed_accuracy, heading_accuracy, course_accuracy);
-
-	// "course": {"speed": 6.0, "heading": 305.880004882812, "heading_accuracy": 5.80999994277954, "speed_accuracy": 8.35999965667725
-	{
-#define APPEND_STRING(s) { int slen = strlen(s); if (space <= slen) goto fail; space -= slen; strcpy(output, s);  output += slen; }
-#define APPEND_SEP { if (!first) APPEND_STRING(", "); }
-#define COU_UPDATE_STATE(r) { if (r < 0 || r >= space) goto fail; space -= r; output += r; first = EFalse; }
-	  int space = COU_STRING_MAX;
-	  int r;
-	  TBool first = ETrue;
-	  gchar* output = couString;
-	  APPEND_STRING("{");
-	  if (!Math::IsNaN(speed)) { r = g_snprintf_fix(output, space, "speed: %.6f", (double)speed); COU_UPDATE_STATE(r); }
-	  if (!Math::IsNaN(heading)) { APPEND_SEP; r = g_snprintf_fix(output, space, "heading: %.6f", (double)heading); COU_UPDATE_STATE(r); }
-	  if (!Math::IsNaN(course_value)) { APPEND_SEP; r = g_snprintf_fix(output, space, "course: %.6f", (double)course_value); COU_UPDATE_STATE(r); }
-	  if (!Math::IsNaN(speed_accuracy)) { APPEND_SEP; r = g_snprintf_fix(output, space, "speed_accuracy: %.6f", (double)speed_accuracy); COU_UPDATE_STATE(r); }
-	  if (!Math::IsNaN(heading_accuracy)) { APPEND_SEP; r = g_snprintf_fix(output, space, "heading_accuracy: %.6f", (double)heading_accuracy); COU_UPDATE_STATE(r); }
-	  if (!Math::IsNaN(course_accuracy)) { APPEND_SEP; r = g_snprintf_fix(output, space, "course_accuracy: %.6f", (double)course_accuracy); COU_UPDATE_STATE(r); }
-	  APPEND_STRING("}");
-	  goto done;
-	fail:
-	  couString[0] = '\0';
-	done: ;
-	  logt(couString);
-	}
+    default: // unknown completion code
+      {
+	guilogf("cellpos: unknown completion code %d", errCode);
+	log_db_log_status(iLogDb, NULL, 
+			  "cellpos: unknown completion code %d", errCode);
       }
-
-      if (positionInfo.PositionClassType() & EPositionInfoClass) {
-	TPosition position;
-	positionInfo.GetPosition(position);
-        // It seems that with network based positioning we only get
-        // lat and lon and hacc, and the rest are NaN.
-	TReal64 lat = position.Latitude();
-	TReal64 lon = position.Longitude();
-	// To use Google Maps to look these up, can use something like http://maps.google.com/maps?q=60.187537,+24.804940&hl=en
-	TReal32 alt = position.Altitude();
-	TReal32 vacc = position.VerticalAccuracy();
-	TReal32 hacc = position.HorizontalAccuracy();
-	//logg("position info: latitude %lg deg, longitude %lg deg, altitude %g m, vertical_accuracy %g m, horizontal_accuracy %g m", lat, lon, alt, vacc, hacc);
-	guilogf("cellpos: lat %lg, lon %lg, alt %g, vacc %g m, hacc %g m", lat, lon, alt, vacc, hacc);
-	if (!Math::IsNaN(lat) &&
-	    !Math::IsNaN(lon)) {
-	  if (!log_db_log_position(iLogDb, lat, lon, alt, vacc, hacc, couString, satString, error)) {
-	    return FALSE;
-	  }
-	} else {
-	  logt("supposedly a full reading has NaN value(s) in coordinates");
-	}
-      }
-    } else {
-      dblogg("warning, unknown gps request status code (%d)", errCode);
     }
   }
 
