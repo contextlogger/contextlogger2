f53b2341297a8a462bd1491f2bbe23e19fa28e35
Tero Hasu <tero.hasu@hut.fi>
2011-02-20 12:10:53 +0200

    sms reader ao builds

---
+++ daemon/src/kr_sms_trigger_epoc.cpp
@@ -0,0 +1,112 @@
+/*
+  References:
+  http://wiki.forum.nokia.com/index.php/SMS_Utilities_API
+*/
+
+#include "kr_sms_trigger_epoc.hpp"
+
+#include <smsustrm.h> // read stream
+
+CTOR_IMPL_CSmsTrigger;
+
+void CSmsTrigger::ConstructL()
+{
+  LEAVE_IF_ERROR_OR_SET_SESSION_OPEN(iSocketServ, iSocketServ.Connect());
+  LEAVE_IF_ERROR_OR_SET_SESSION_OPEN(iSocket, 
+				     iSocket.Open(iSocketServ, 
+						  KSMSAddrFamily, 
+						  KSockDatagram, 
+						  KSMSDatagramProtocol));
+  _LIT8(KTag, "##cl");
+  iSmsAddr.SetSmsAddrFamily(ESmsAddrMatchText); 
+  iSmsAddr.SetTextMatch(KTag);
+  User::LeaveIfError(iSocket.Bind(iSmsAddr));
+}
+
+CSmsTrigger::~CSmsTrigger()
+{
+  Cancel();
+  SESSION_CLOSE_IF_OPEN(iSocket);
+  SESSION_CLOSE_IF_OPEN(iSocketServ);
+}
+
+void CSmsTrigger::RunL()
+{
+  TInt errCode = iStatus.Int();
+  if (errCode) {
+    RunError(errCode);
+    return;
+  }
+  CSmsBuffer *smsBuffer = CSmsBuffer::NewL();
+  CleanupStack::PushL(smsBuffer);
+  // see enum TSmsPDUType
+  CSmsMessage* message = CSmsMessage::NewL(iFs, CSmsPDU::ESmsDeliver, 
+					   smsBuffer);
+  CleanupStack::PushL(message);
+  {
+    RSmsSocketReadStream readStream(iSocket);
+    CleanupClosePushL(readStream);
+    message->InternalizeL(readStream);
+    CleanupStack::PopAndDestroy(); // readStream
+  }
+  TInt bufLen = smsBuffer->Length(); // number of characters
+  RBuf des;
+  des.CreateL(bufLen);
+  des.CleanupClosePushL();
+  smsBuffer->Extract(des, 0, bufLen);
+
+  logg("control message: '%S'", &des);
+
+  CleanupStack::PopAndDestroy(3); // des, message, smsBuffer
+}
+
+void CSmsTrigger::DoCancel()
+{
+  iSocket.CancelIoctl();
+}
+
+TInt CSmsTrigger::RunError(TInt errCode)
+{
+  TRAPD(leaveCode, iObserver.SmsTriggerErrorL(errCode));
+  if (leaveCode)
+    iObserver.SmsTriggerLeave(leaveCode);
+  return 0;
+}
+
+void CSmsTrigger::MakeRequest()
+{
+  if (IsActive())
+    Cancel();
+  iPckgBuf() = KSockSelectRead;
+  iSocket.Ioctl(KIOctlSelect, iStatus, &iPckgBuf, KSOLSocket);
+  SetActive();
+}
+
+/**
+
+Copyright 2011 Helsinki Institute for Information Technology (HIIT)
+and the authors. All rights reserved.
+
+Authors: Tero Hasu <tero.hasu@hut.fi>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ **/
---
+++ daemon/src/kr_sms_trigger_epoc.hpp
@@ -0,0 +1,115 @@
+#ifndef __kr_sms_trigger_epoc_hpp__
+#define __kr_sms_trigger_epoc_hpp__
+
+#include "ac_app_context.h"
+#include "utils_cl2.h"
+
+#include <e32base.h>
+#include <es_sock.h>
+#include <gsmubuf.h>
+#include <gsmumsg.h>
+#include <smsuaddr.h>
+
+NONSHARABLE_CLASS(MSmsTrigger)
+{
+ public:
+  virtual void SmsTriggerReceivedL(TInt aSecs) = 0;
+  virtual void SmsTriggerErrorL(TInt errCode) = 0;
+  virtual void SmsTriggerLeave(TInt errCode) = 0;
+};
+
+/***koog 
+(require racket/class)
+(require codegen/symbian-ctor)
+
+(ctor-defines (new 
+  (class ctor%
+    (super-new)
+    (define/override (class-name) "CSmsTrigger")
+    (define/override (args/string) "ac_AppContext* aAppContext, MSmsTrigger& aObserver")
+    (define/override (init-expr) "CActive(EPriorityStandard), iAppContext(aAppContext), iObserver(aObserver), iFs(ac_Fs(aAppContext))")
+    (define/override (ctor-code) "CActiveScheduler::Add(this);")
+   )))
+ ***/
+#define CTOR_DECL_CSmsTrigger  \
+public: static CSmsTrigger* NewLC(ac_AppContext* aAppContext, MSmsTrigger& aObserver); \
+public: static CSmsTrigger* NewL(ac_AppContext* aAppContext, MSmsTrigger& aObserver); \
+private: CSmsTrigger(ac_AppContext* aAppContext, MSmsTrigger& aObserver); \
+private: void ConstructL();
+
+#define CTOR_IMPL_CSmsTrigger  \
+CSmsTrigger* CSmsTrigger::NewLC(ac_AppContext* aAppContext, MSmsTrigger& aObserver) \
+{ \
+  CSmsTrigger* obj = new (ELeave) CSmsTrigger(aAppContext, aObserver); \
+  CleanupStack::PushL(obj); \
+  obj->ConstructL(); \
+  return obj; \
+} \
+ \
+CSmsTrigger* CSmsTrigger::NewL(ac_AppContext* aAppContext, MSmsTrigger& aObserver) \
+{ \
+  CSmsTrigger* obj = CSmsTrigger::NewLC(aAppContext, aObserver); \
+  CleanupStack::Pop(obj); \
+  return obj; \
+} \
+ \
+CSmsTrigger::CSmsTrigger(ac_AppContext* aAppContext, MSmsTrigger& aObserver) : CActive(EPriorityStandard), iAppContext(aAppContext), iObserver(aObserver), iFs(ac_Fs(aAppContext)) \
+{CActiveScheduler::Add(this);}
+/***end***/
+
+NONSHARABLE_CLASS(CSmsTrigger) : 
+  public CActive
+{
+  CTOR_DECL_CSmsTrigger;
+
+ public:
+  ~CSmsTrigger();
+
+ private:
+  ac_AppContext* iAppContext;
+  MSmsTrigger& iObserver;
+  RFs& iFs;
+  DEF_SESSION(RSocketServ, iSocketServ);
+  DEF_SESSION(RSocket, iSocket);
+  TSmsAddr iSmsAddr;
+  TPckgBuf<TUint> iPckgBuf;
+
+ private: // CActive
+  virtual void RunL();
+  virtual void DoCancel();
+  virtual TInt RunError(TInt errCode);
+
+ public:
+  void MakeRequest(); // one-shot request
+};
+
+#endif /* __kr_sms_trigger_epoc_hpp__ */
+
+/**
+
+Copyright 2011 Helsinki Institute for Information Technology (HIIT)
+and the authors. All rights reserved.
+
+Authors: Tero Hasu <tero.hasu@hut.fi>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ **/
